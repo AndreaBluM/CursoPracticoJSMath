1ea32bf175102cf3fe3d6d57c72cd47de155eb15
Andrea <lauralme96@gmail.com>
2023-02-07 16:35:23 -0500

    codigo ordenado

--- js/math.js
+++ js/math.js
@@ -1,3 +1,4 @@
+ //Cuadrado
  const ladoCuadrado = 6;
  const perimetroCuadrado = ladoCuadrado * 4;
  const areaPerimetro = ladoCuadrado * ladoCuadrado ;
@@ -9,30 +10,26 @@
     areaPerimetro2
  })
 
+//Triangulo
  const ladoTriangulo1 = 6;
  const ladoTriangulo2 = 6;
  const ladoTriangulo3 = 4;
  const altura = 5.5;
-
- const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTriangulo3;
+   const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTriangulo3;
  const areaTriangulo = (ladoTriangulo3 * altura) / 2;
 
- function calcularAreaTriangulo(base, altura){
+function calcularAreaTriangulo(base, altura){
     return (base * altura) / 2;
  }
-
-
- function calcularAlturaTrianguloisoceles(lado1, base){
+function calcularAlturaTrianguloisoceles(lado1, base){
    if(lado1 == base){
       console.warn('Este triangulo no es isoceles')
    } else{
       // h = raizcuadrada(Math.pow(lado1,2) - ((Math.pow(base,2)/4) )
       return Math.sqrt(Math.pow(lado1,2) - ((Math.pow(base,2)/4)))
    }
-
- }
-
- function calcularAlturaEscaleno (lado1, lado2, lado3){
+}
+function calcularAlturaEscaleno (lado1, lado2, lado3){
    const semiPerimetro = (lado1 + lado2 + lado3) / 2;
    if(lado1 == lado2 && lado1 == lado3 ){
       console.warn('Este no es un triangulo escaleno')
@@ -40,16 +37,16 @@
       //h = a/2 raizcuadradaS(S−a)(S−b)(S−c)
      return (2/lado1)*Math.sqrt((semiPerimetro*(semiPerimetro-lado1)*(semiPerimetro-lado2)*(semiPerimetro-lado3)))
    }
- }
+}
  
 
 calcularAreaTriangulo(4, 5.5)
 
+//Circulo
 console.group('circulo')
 
 const radioCirculo = 3;
 const diametroCirculo = radioCirculo * 2;
-
 const circunferencia = diametroCirculo * Math.PI;
 const areaCirculo = Math.pow(radioCirculo,2) * Math.PI;
 
@@ -65,11 +62,8 @@ function CalcularCirculo (radio){
       circunferencia: (radio * 2) * Math.PI,
       area: Math.pow(radio,2) * Math.PI,
    };
-
 }
-
 CalcularCirculo(3)
-
 console.groupEnd('circulo')
 
 
