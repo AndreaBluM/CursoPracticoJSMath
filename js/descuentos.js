4db4d00be647aa48426a6595a2b406866612dc37
Andrea <lauralme96@gmail.com>
2023-02-07 22:21:39 -0500

    Pagina de descuentos lista

--- js/descuentos.js
+++ js/descuentos.js
@@ -7,15 +7,30 @@ const precioValue = document.querySelector('#preciovalue');
 const descuentoValue = document.querySelector('#descuentovalue');
 const boton = document.querySelector('#calcular')
 const resultado = document.querySelector('#preciocondescuento')
+const cupon = document.querySelector('#cupondescuento')
 
 boton.addEventListener('click', calculardescuento)
 
 function calculardescuento(){
- const price = precioValue.value;
- const descount = descuentoValue.value;
+ const price = Number(precioValue.value);
+ const descount = Number(descuentoValue.value);
+ const cupon2 = cupon.value
+ 
+ if (descount > 100){
+    resultado.innerText = 'El descuento tiene que ser menor a 100';
+    return
+ }
+ if(cupon2 === "Cupon2020"){
+    const result = (price * (100-descount))/100;
+    resultado.innerText = result;
+ } else if(cupon2 === ""){
+    const result = (price * (100-descount))/100;
+    resultado.innerText = result;
+ } else{
+    resultado.innerText = 'Este cupon no es correcto';
+ }
 
- const result = (price * (100-descount))/100;
- resultado.innerText = result;
+ 
 
 }
 
